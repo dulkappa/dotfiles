77b9b6f52577793039a0e64c3bd5f1c42ff10f61
dulkappa <dulkappa@gmail.com>
2014-09-06 23:26:32 +0900

    Modify init.el (font settings)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -40,31 +40,41 @@
 (which-function-mode 1)
 
 ;;; font settings
-(set-fontset-font
- (frame-parameter nil 'font)
- 'japanese-jisx0208
- '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-(set-fontset-font
- (frame-parameter nil 'font)
- 'japanese-jisx0212
- '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-(set-fontset-font
- (frame-parameter nil 'font)
- 'mule-unicode-0100-24ff
- '("monaco" . "iso10646-1"))
-(setq face-font-rescale-alist
-      '(("^-apple-hiragino.*" . 1.2)
-        (".*osaka-bold.*" . 1.2)
-        (".*osaka-medium.*" . 1.2)
-        (".*courier-bold-.*-mac-roman" . 1.0)
-        (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-        (".*monaco-bold-.*-mac-roman" . 0.9)
-        ("-cdac$" . 1.3)))
+;;; -*- mode: emacs-lisp; coding: utf-8-emacs; -*-
 
-(create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
-(set-fontset-font "fontset-menlokakugo"
-                  'unicode
-                  (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
-                  nil
-                  'append)
-(add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+(if (and (locate-library "fontset")
+         (fboundp 'add-to-list)
+         (fboundp 'display-graphic-p))
+    (progn
+      (cond
+       ((and (string-match "linux-gnu" system-configuration)
+             (display-graphic-p))
+        (add-to-list 'default-frame-alist '(font . "TakaoGothic-12"))
+        (add-to-list 'initial-frame-alist '(font . "TakaoGothic-12"))
+        (add-hook 'after-init-hook
+                  (lambda () (set-frame-font "TakaoGothic-12"))))
+       ((and (string-match "mingw-nt" system-configuration)
+             (display-graphic-p))
+        (add-to-list 'default-frame-alist '(font . "MS Gothic-12"))
+        (add-to-list 'initial-frame-alist '(font . "MS Gothic-12"))
+        (add-hook 'after-init-hook
+                  (lambda () (set-frame-font "MS Gothic-12"))))
+       ((and (string-match "apple-darwin" system-configuration)
+             (display-graphic-p))
+        (create-fontset-from-ascii-font
+         "Menlo-12:weight=normal:slant=normal"
+         nil
+         "menlokakugo")
+        (set-fontset-font
+         "fontset-menlokakugo"
+         'unicode
+         (font-spec :family "Hiragino Kaku Gothic ProN" :size 12)
+         nil
+         'append)
+        (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+        (add-to-list 'initial-frame-alist '(font . "fontset-menlokakugo"))
+        (add-to-list 'face-font-rescale-alist 
+                     '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+        (add-hook 'after-init-hook
+                  (lambda () (set-frame-font "fontset-menlokakugo")))))))
+
